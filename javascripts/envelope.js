1ceba3ba7c980c60a57aec5e8393419aea8883f7
Nicolas Laplante <nicolas.laplante@gmail.com>
2015-01-07 06:17:19 -0500

    start new site version

---
+++ javascripts/envelope.js
@@ -0,0 +1,12 @@
+/*
+ * envelope.js
+ *
+ * support script for nlaplante.github.io/envelope
+ *
+ * depends on jQuery, bootstrap
+ */
+(function ($, w, undefined) { 'use strict';
+
+
+
+} (jQuery, window));
